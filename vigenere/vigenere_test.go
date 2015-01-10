3d005301c79224a0db88a5295071f88b139259d4
Harihar Subramanyam <hahasubbu@gmail.com>
2015-01-09 19:45:56 -0500

    Tests

--- vigenere/vigenere_test.go
+++ vigenere/vigenere_test.go
@@ -5,6 +5,24 @@ import (
 	"testing"
 )
 
-func TestSomething(t *testing.T) {
-	assert.Equal(t, 1, 1, "They should be equal")
+func TestSuccessfulEncode(t *testing.T) {
+	actual, err := Encode(1234, "password")
+	assert.Nil(t, err)
+	assert.Equal(t, "qcvwxquh", actual, "Encoding doesn't work.")
+}
+
+func TestInvalidMessage(t *testing.T) {
+	_, err := Encode(1234, "PASSWORD")
+	assert.NotNil(t, err)
+}
+
+func TestLongKey(t *testing.T) {
+	actual, err := Encode(1111, "pa")
+	assert.Nil(t, err)
+	assert.Equal(t, "qb", actual, "Long key encoding doesn't work.")
+}
+
+func TestInvalidCharacter(t *testing.T) {
+	_, err := Encode(1111, "p!")
+	assert.NotNil(t, err)
 }
